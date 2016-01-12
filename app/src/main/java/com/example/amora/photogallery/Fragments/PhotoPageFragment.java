16b627e8974e0d45c27c4d57839723c544bf2c62
Adam Merritt <amerrit2@gmail.com>
2016-01-11 23:44:10 -0500

    Finished

--- app/src/main/java/com/example/amora/photogallery/Fragments/PhotoPageFragment.java
+++ app/src/main/java/com/example/amora/photogallery/Fragments/PhotoPageFragment.java
@@ -6,8 +6,11 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.example.amora.photogallery.Activities.R;
 
@@ -32,6 +35,10 @@ public class PhotoPageFragment extends VisibleFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_photo_page, container, false);
 
+        final ProgressBar progressBar = (ProgressBar) v.findViewById(R.id.progressBar);
+        progressBar.setMax(100);
+        final TextView titleTextView = (TextView) v.findViewById(R.id.titleTextView);
+
         mWebView = (WebView) v.findViewById(R.id.webView);
 
         mWebView.getSettings().setJavaScriptEnabled(true);
@@ -40,6 +47,23 @@ public class PhotoPageFragment extends VisibleFragment {
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
                 return false;
+            }
+        });
+
+        mWebView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                if (newProgress == 100) {
+                    progressBar.setVisibility(View.INVISIBLE);
+                } else {
+                    progressBar.setVisibility(View.VISIBLE);
+                    progressBar.setProgress(newProgress);
+                }
+            }
+
+            @Override
+            public void onReceivedTitle(WebView view, String title) {
+                titleTextView.setText(title);
             }
         });
 
